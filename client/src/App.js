120c5e2157774a08bd9eef4db7f9ab5dd8dfe6ca
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-08-11 19:35:30 -0700

    creating return statement
    added index.html to return func to populate courses to webpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+//import logo from './logo.svg';
 import './App.css';
 import Courses from './Components/Courses';
 
@@ -14,29 +16,52 @@ import {
 //       )
 //     };
 
-   
-  
-
-
 function App() {
+  const [ courses, setCourses ] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="wrap main--grid">
+                <a className="course--module course--link" href="course-detail.html">
+                    <h2 className="course--label">Course</h2>
+                    <h3 className="course--title">Build a Basic Bookcase</h3>
+                </a>
+                <a className="course--module course--link" href="course-detail.html">
+                    <h2 className="course--label">Course</h2>
+                    <h3 className="course--title">Learn How to Program</h3>
+                </a>
+                <a className="course--module course--link" href="course-detail.html">
+                    <h2 className="course--label">Course</h2>
+                    <h3 className="course--title">Learn How to Test Programs</h3>
+                </a>
+                <a className="course--module course--add--module" href="create-course.html">
+                    <span className="course--add--title">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                        viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
+                        New Course
+                    </span>
+                </a>
+            </div>
+   
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
+
+// fetchData = (useState) => {
+//   fetch ()
 
 export default App;
--- client/src/Components/Courses.js
+++ client/src/Components/Courses.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
+import ReactDOM from 'react-dom/client';
 
+//import '../App.css';
 
 function Courses() {
-    const [ courses, setCourses ] = useState(0);
-   
+   const [ courses, setCourses ] = useState(0);
+
+   return <h1> { courses }</h1>;
+
+   useState((courseName) => {});
+
+   useEffect(() => {
+     console.log("hello from useEffect");
+   });
 }
 
-useState(() => {
-
-})
-
-useEffect(() => {
-    console.log("hello from useEffect");
-})
-
+export default Courses;
