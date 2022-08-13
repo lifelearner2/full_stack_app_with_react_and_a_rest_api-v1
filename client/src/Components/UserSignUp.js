147e6ecd53d3cedbbf91c02d6d63836ca7f4eb1e
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-08-13 14:03:10 -0700

    create usersignup component
    added comment

---
+++ client/src/Components/UserSignUp.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+//may do something similar below for creating a new user
+// submit = () => {
+//     const { context } = this.props;
+//     const {
+//       name,
+//       username,
+//       password,
+//     } = this.state; 
+      
+//       // New user payload
+//       const user = {
+//         name,
+//         username,
+//         password,
+//       };
+//       context.data.createUser(user)
+//       .then(errors => {
+//     if (errors.length) {
+//       this.setState( { errors });
+//     } else {
+//       console.log(`${username} is successfully signed up and authenticated!`);
+//     }
+//       })
+//       cancel = () => {
+    
+//       };
+//     }
