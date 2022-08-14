a87f0ced8710ac685a9193b1464df4baf0205f29
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-08-13 17:27:38 -0700

    Update UserSignOut.js

--- client/src/Components/UserSignOut.js
+++ client/src/Components/UserSignOut.js
@@ -1,5 +1,6 @@
 //This component signs out the authenticated user and redirects the user to the default route (i.e. the list of courses).
-
+// import React from 'react';
+// import { Redirect } from 'react-router-dom';
 
 // export default () => {
 //     return (
