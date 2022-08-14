16e99e1180c3bf7d2ca8491308f29ddd53eeac27
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-08-13 23:06:44 -0700

    Create PrivateRoute.js

---
+++ client/src/PrivateRoute.js
@@ -0,0 +1,18 @@
+//This is a higher-order component (HOC) that is used to configure protected routes (i.e. routes that require authentication).
+
+// import React from 'react';
+// import { Route, Redirect } from 'react-router-dom';
+// import { Consumer } from './Context';
+
+// export default ({ component: Component, ...rest }) => {
+//   return (
+//     <Consumer>
+//       { context => (
+//         <Route
+//           {...rest}
+//           render={}
+//         />
+//       )}
+//     </Consumer>
+//   );
+// };
