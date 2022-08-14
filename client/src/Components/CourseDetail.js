6938a9e56ceab7349b01fb98b1e1627f428ddf17
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-08-13 17:42:25 -0700

    Update CourseDetail.js

--- client/src/Components/CourseDetail.js
+++ client/src/Components/CourseDetail.js
@@ -2,6 +2,17 @@
 //This component also renders a "Delete Course" button that when clicked should send a DELETE request to the REST API's /api/courses/:id route in order to delete a course.
 //This component also renders an "Update Course" button for navigating to the "Update Course" screen.
 
+//'use strict';
+//update routes appropriately
+//const express = require('express');
+//const { asyncHandler } = require('./middleware/async-handler');
+//const { Course or Courses } = require('./'); //do I require this from the api folder or client 
+//const { authenticateUser } = require('./middleware/auth-user');
+
+// Construct a router instance.
+//const router = express.Router();
+
+
 //GET courses Route | route that will return all courses including User associated w/each course - along with a 200 HTTP status code.
 // router.get('/courses', asyncHandler(async (req, res) => {
 //     let courses = await Course.findAll({
