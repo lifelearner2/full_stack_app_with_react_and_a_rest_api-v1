4f0b8547302cfc9e7051e8bad1d0e87c86dda985
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-08-13 17:38:25 -0700

    Update CourseDetail.js

--- client/src/Components/CourseDetail.js
+++ client/src/Components/CourseDetail.js
@@ -2,21 +2,13 @@
 //This component also renders a "Delete Course" button that when clicked should send a DELETE request to the REST API's /api/courses/:id route in order to delete a course.
 //This component also renders an "Update Course" button for navigating to the "Update Course" screen.
 
-
-
-
-//PUT route that updates corresponding course if the user owns it and returns a 204 code
-// router.put('/courses/:id', authenticateUser, asyncHandler(async(req, res, next) => {
-//     const course = await Course.findByPk(req.params.id);
-//     const user = await req.currentUser;
-//     if (course.userId === user.id) {
-//       await course.update(req.body);
-//       res.status(204).end();
-//     } else {
-//      next();
-//     }
+//GET courses Route | route that will return all courses including User associated w/each course - along with a 200 HTTP status code.
+// router.get('/courses', asyncHandler(async (req, res) => {
+//     let courses = await Course.findAll({
+//       include: {model: User}
+//     });
+//     res.json(courses);
 //   }));
-
 
 
 //DELETE route that deletes corresponding course if the user owns it and returns a 204 code
@@ -28,5 +20,18 @@
 //       res.status(204).end();
 //     } else {
 //       next();
+//     }
+//   }));
+
+
+//PUT route that updates corresponding course if the user owns it and returns a 204 code
+// router.put('/courses/:id', authenticateUser, asyncHandler(async(req, res, next) => {
+//     const course = await Course.findByPk(req.params.id);
+//     const user = await req.currentUser;
+//     if (course.userId === user.id) {
+//       await course.update(req.body);
+//       res.status(204).end();
+//     } else {
+//      next();
 //     }
 //   }));
