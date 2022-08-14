497531666a91ae36c8dfb90e72504d73d68ef94b
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-08-13 19:22:37 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -62,7 +62,7 @@ export default App;
 //GET courses Route | route that will return all courses including User associated w/each course - along with a 200 HTTP status code.
 // app.get('/courses', asyncHandler(async (req, res) => {
 //   let courses = await Course.findAll({
-//     include: {model: User}
+//     include: {model: User} //not sure about including this
 //   });
 //   res.json(courses);
 // }));
@@ -70,14 +70,14 @@ export default App;
 //POST route that creates new course, sets location header to URI for new course & returns a 201 code
 // app.post('/courses/create', authenticateUser, asyncHandler(async(req, res) => {
 //   const course = await Course.create(req.body);
-//   res.status(201).location(`/courses/${course.id}`).end();
+//   res.status(201).location(`/courses/create/${course.id}`).end();
 // }));
 
 //PUT route that updates corresponding course if the user owns it and returns a 204 code
 // app.put('/courses/:id/update', authenticateUser, asyncHandler(async(req, res, next) => {
 //   const course = await Course.findByPk(req.params.id);
 //   const user = await req.currentUser;
-//   if (course.userId === user.id) {
+//   if (course.userId === user.id) {  //need to change userID?
 //     await course.update(req.body);
 //     res.status(204).end();
 //   } else {
@@ -89,7 +89,7 @@ export default App;
 //GET route that returns corresponding course incl. User assoc w/course & 200 code
 // app.get("/courses/:id", asyncHandler(async (req, res, next) => {
 //   const course = await Course.findByPk(req.params.id, {
-//     include: {model: User}
+//     include: {model: User} //get rid of this?
 //   }); 
 //   course ? res.json(course) : next();
 // }));
