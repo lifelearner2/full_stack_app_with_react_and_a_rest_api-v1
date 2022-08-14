c782e2769a787e8ef715c3db96e74dd886e766ba
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-08-13 19:18:57 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from 'react-dom/client';
+//import { Link } from 'react-router-dom';
 //import axios from 'axios';
 import Header from './Components/Header';
 import Courses from './Components/Courses';
-
+// import CreateCourse from './Components/CreateCourse';
+// import UpdateCourse from './Components/UpdateCourse';
+// import CourseDetail from './Components/CourseDetail';
+// import UserSignIn from './Components/UserSignIn';
+// import UserSignUp from './Components/UserSignUp';
+// import UserSignOut from './Components/UserSignOut';
 
 
  import {
@@ -51,3 +57,41 @@ export default App;
 // /signin - UserSignIn
 // /signup - UserSignUp
 // /signout - UserSignOut
+
+
+//GET courses Route | route that will return all courses including User associated w/each course - along with a 200 HTTP status code.
+// app.get('/courses', asyncHandler(async (req, res) => {
+//   let courses = await Course.findAll({
+//     include: {model: User}
+//   });
+//   res.json(courses);
+// }));
+
+//POST route that creates new course, sets location header to URI for new course & returns a 201 code
+// app.post('/courses/create', authenticateUser, asyncHandler(async(req, res) => {
+//   const course = await Course.create(req.body);
+//   res.status(201).location(`/courses/${course.id}`).end();
+// }));
+
+//PUT route that updates corresponding course if the user owns it and returns a 204 code
+// app.put('/courses/:id/update', authenticateUser, asyncHandler(async(req, res, next) => {
+//   const course = await Course.findByPk(req.params.id);
+//   const user = await req.currentUser;
+//   if (course.userId === user.id) {
+//     await course.update(req.body);
+//     res.status(204).end();
+//   } else {
+//    next();
+//   }
+// }));
+
+
+//GET route that returns corresponding course incl. User assoc w/course & 200 code
+// app.get("/courses/:id", asyncHandler(async (req, res, next) => {
+//   const course = await Course.findByPk(req.params.id, {
+//     include: {model: User}
+//   }); 
+//   course ? res.json(course) : next();
+// }));
+
+
