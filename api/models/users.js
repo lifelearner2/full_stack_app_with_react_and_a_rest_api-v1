79c23440bcec003e52bc308a0207d25efe3fd839
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-08-17 09:38:29 -0700

    Update users.js

--- api/models/users.js
+++ api/models/users.js
@@ -64,8 +64,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       set(val) {
+        if(val) {
         const hashedPassword = bcrypt.hashSync(val, 10);
         this.setDataValue('password', hashedPassword);
+        }
       },
       validate: {
         notNull: {
